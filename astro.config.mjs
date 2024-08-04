95ae28f766e11bba344051bd351cb8e8d79f8beb
joe vaughan <jbvaughan1993@gmail.com>
2024-08-04 13:23:42 +0100

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import starlight from '@astrojs/starlight'
-
 import react from '@astrojs/react'
 
 // https://astro.build/config
@@ -17,11 +16,31 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      '/wp-admin': 'https://adamkoszary.co.uk',
-      '/wp-content': 'https://adamkoszary.co.uk',
-      '/wp-includes': 'https://adamkoszary.co.uk',
-      '/wp-json': 'https://adamkoszary.co.uk',
-      '/wp-login.php': 'https://adamkoszary.co.uk'
+      '/wp-admin': {
+        target: 'https://adamkoszary.co.uk',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/wp-admin/, '/wp-admin')
+      },
+      '/wp-content': {
+        target: 'https://adamkoszary.co.uk',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/wp-content/, '/wp-content')
+      },
+      '/wp-includes': {
+        target: 'https://adamkoszary.co.uk',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/wp-includes/, '/wp-includes')
+      },
+      '/wp-json': {
+        target: 'https://adamkoszary.co.uk',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/wp-json/, '/wp-json')
+      },
+      '/wp-login.php': {
+        target: 'https://adamkoszary.co.uk',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/wp-login\.php/, '/wp-login.php')
+      }
     }
   }
 })
