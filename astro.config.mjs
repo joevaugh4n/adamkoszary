9a28eb69ffde953f4ea07f80b8ea2f60920d7af3
joe vaughan <jbvaughan1993@gmail.com>
2024-08-04 13:05:29 +0100

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,27 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
-import starlight from "@astrojs/starlight";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
+import starlight from '@astrojs/starlight'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), starlight({
-    title: "Documentation"
-  }), react()]
-});
+  integrations: [
+    tailwind(),
+    icon(),
+    starlight({
+      title: 'Documentation'
+    }),
+    react()
+  ],
+  server: {
+    proxy: {
+      '/wp-admin': 'https://adamkoszary.co.uk',
+      '/wp-content': 'https://adamkoszary.co.uk',
+      '/wp-includes': 'https://adamkoszary.co.uk',
+      '/wp-json': 'https://adamkoszary.co.uk',
+      '/wp-login.php': 'https://adamkoszary.co.uk'
+    }
+  }
+})
