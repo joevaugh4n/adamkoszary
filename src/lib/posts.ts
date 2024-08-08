fd34d380278d8a2644c450d1c4a5f3a87f86949f
joe vaughan <jbvaughan1993@gmail.com>
2024-08-08 15:49:55 +0100

    small tweaks to logic

---
+++ src/lib/posts.ts
@@ -0,0 +1,120 @@
+// blog.service.ts
+import { wpquery } from "./wordpress";
+
+interface Tag {
+  name: string;
+  slug: string;
+  count: number;
+}
+
+interface PostProps {
+  dateGmt: string;
+  title: string;
+  slug: string;
+  excerpt: string;
+  tags: { nodes: Tag[] };
+}
+
+interface DataProps {
+  posts: {
+    nodes: PostProps[];
+  };
+}
+
+export async function fetchBlogPosts(): Promise<DataProps> {
+  try {
+    const response = await wpquery({
+      query: `
+        query GetPostExcerptsAndAllTags {
+          posts {
+            nodes {
+              title
+              dateGmt
+              excerpt
+              slug
+              featuredImage {
+                node {
+                  mediaItemUrl
+                  altText
+                }
+              }
+              tags {
+                nodes {
+                  name
+                  slug
+                  count
+                }
+              }
+            }
+          }
+        }
+      `,
+    });
+
+    if (response.posts?.nodes) {
+      return response as DataProps;
+    } else {
+      console.error("No posts found in the response", response);
+      return { posts: { nodes: [] } };
+    }
+  } catch (error) {
+    console.error("Error fetching post data:", error);
+    return { posts: { nodes: [] } };
+  }
+}
+
+export async function fetchLatestPost(): Promise<PostProps> {
+  try {
+    const response = await wpquery({
+      query: `
+        query GetLatestPost {
+          posts(first: 1) {
+            nodes {
+              title
+              dateGmt
+              slug
+              excerpt
+              tags {
+                nodes {
+                  name
+                  slug
+                  count
+                }
+              }
+            }
+          }
+        }
+      `,
+    });
+
+    if (response.posts?.nodes?.[0]) {
+      return response.posts.nodes[0];
+    } else {
+      console.error("No posts found in the response", response);
+      return {
+        dateGmt: '',
+        title: '',
+        slug: '',
+        excerpt: '',
+        tags: { nodes: [] },
+      };
+    }
+  } catch (error) {
+    console.error("Error fetching latest post data:", error);
+    return {
+      dateGmt: '',
+      title: '',
+      slug: '',
+      excerpt: '',
+      tags: { nodes: [] },
+    };
+  }
+}
+
+export function getUniqueTags(posts: PostProps[]): Tag[] {
+  const tagsSet = new Set();
+  posts.forEach((post) => {
+    post.tags.nodes.forEach((tag) => tagsSet.add(JSON.stringify(tag)));
+  });
+  return Array.from(tagsSet).map((tag: any) => JSON.parse(tag));
+}
