f272020684d2d9849a52e1cf611608928e553920
Joe Vaughan <jbvaughan1993@gmail.com>
2024-08-08 10:47:12 +0100

    fonts optimisation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,11 @@
-import starlightPlugin from "@astrojs/starlight-tailwind";
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "!./src/content/docs/*",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         sans: "Inter",
-        mono: "IBM Plex Mono",
+        mono: ["IBM Plex Mono", "Monospace"],
         handwriting: "Permanent Marker",
       },
       colors: {
