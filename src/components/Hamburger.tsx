4059fd7f9bf789b00726717ffbfb7df97fba0e90
joe vaughan <jbvaughan1993@gmail.com>
2025-05-25 14:53:58 +0100

    fixes

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -1,5 +1,7 @@
 import burger from '../images/burger.svg'
+import burgerWhite from '../images/burger-white.svg'
 import close from '../images/close.svg'
+import closeWhite from '../images/close-white.svg'
 import { useState } from 'react'
 
 interface BurgerProps {
@@ -9,8 +11,9 @@ interface BurgerProps {
 
 export function Burger({ setState, state }: BurgerProps) {
     return (
-        <button onClick={() => setState(!state)}>
-            <img src={burger.src} className='h-10' />
+        <button className='cursor-pointer' onClick={() => setState(!state)}>
+            <img className='dark:hidden h-10' src={burger.src} />
+            <img className='not-dark:hidden h-10' src={burgerWhite.src} />
         </button>
     )
 }
@@ -23,7 +26,8 @@ interface ButtonProps {
 export function Close({ setState, state }: ButtonProps) {
     return (
         <button onClick={() => setState(!state)}>
-            <img src={close.src} className='h-8' />
+            <img className='dark:hidden h-8' src={close.src} />
+            <img className='not-dark:hidden h-8' src={closeWhite.src} />
         </button>
     )
 }
@@ -54,16 +58,16 @@ export function List({ state, setState }: ButtonProps) {
         },
     ]
     return (
-        <ul className='flex flex-col items-end gap-2'>
+        <ul className='flex flex-col items-end gap-8 pt-8'>
             {ListItems.map((item) =>
-                <li key={item.id}>
-                    <a href={item.url} title={item.string}>
-                        <button
-                            className='text-xl rounded-lg px-8 py-4 tracking-tight transition-colors duration-300 font-mono bg-slate-200 text-black hover:bg-black hover:text-white dark:hover:bg-slate-500'>
-                            {item.string}
-                        </button>
+                <li key={item.id} >
+                    <a href={item.url} title={item.string}
+                        className='text-xl rounded-lg px-8 py-4 tracking-tight transition-colors duration-300 font-mono bg-slate-200 text-black hover:bg-black hover:text-white dark:hover:bg-slate-500'>
+                        {item.string}
                     </a>
-                </li >)
+
+                </li >
+            )
             }
             <li><Close state={state} setState={setState} />
             </li>
